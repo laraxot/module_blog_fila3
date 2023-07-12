f7b67728db93130e4941bd864def0e3b25898d6a
marco76tv <marco.sottana@gmail.com>
2023-07-12 12:33:34 +0200

    up

--- Providers/FilamentServiceProvider.php
+++ Providers/FilamentServiceProvider.php
@@ -1,18 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Blog\Providers;
 
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationItem;
 use Savannabits\FilamentModules\ContextServiceProvider;
 
-class FilamentServiceProvider extends ContextServiceProvider
-{
+class FilamentServiceProvider extends ContextServiceProvider {
     public static string $name = 'blog-filament';
     public static string $module = 'Blog';
 
-    public function packageRegistered(): void
-    {
+    public function packageRegistered(): void {
         $this->app->booting(function () {
             $this->registerConfigs();
         });
@@ -21,22 +21,21 @@ class FilamentServiceProvider extends ContextServiceProvider
 
     public function registerConfigs() {
         $this->mergeConfigFrom(
-            app('modules')->findOrFail(static::$module)->getExtraPath( 'Config/'.static::$name.'.php'),
+            app('modules')->findOrFail(static::$module)->getExtraPath('Config/'.static::$name.'.php'),
             static::$name
         );
     }
 
-    public function boot()
-    {
+    public function boot() {
         parent::boot();
         Filament::serving(function () {
-            Filament::forContext('filament', function (){
+            Filament::forContext('filament', function () {
                 Filament::registerNavigationItems([
-                    NavigationItem::make(static::$module)->label(static::$module.' Module')->url(route(static::$name.'.pages.dashboard'))->icon('heroicon-o-bookmark')->group('Modules')
+                    NavigationItem::make(static::$module)->label(static::$module.' ')->url(route(static::$name.'.pages.dashboard'))->icon('heroicon-o-bookmark')->group('Modules'),
                 ]);
             });
-            Filament::forContext(static::$name, function (){
-                Filament::registerRenderHook('sidebar.start',fn():string => \Blade::render('<div class="p-2 px-6 bg-primary-100 font-black w-full">'.static::$module.' Module</div>'));
+            Filament::forContext(static::$name, function () {
+                Filament::registerRenderHook('sidebar.start', fn (): string => \Blade::render('<div class="p-2 px-6 bg-primary-100 font-black w-full">'.static::$module.' </div>'));
             });
         });
     }
