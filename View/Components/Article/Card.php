20b0ef6e2856850b38e51b987d59715fe86cbc38
marco76tv <marco.sottana@gmail.com>
2023-11-24 09:45:10 +0100

    Add Article Card component with v1 template

--- View/Components/Article/Card.php
+++ View/Components/Article/Card.php
@@ -4,19 +4,34 @@ declare(strict_types=1);
 
 namespace Modules\Blog\View\Components\Article;
 
+use Illuminate\Contracts\Support\Renderable;
 use Illuminate\View\Component;
+use Modules\Blog\Models\Post as Article;
+use Modules\Xot\Actions\GetViewAction;
 
 // use Modules\Xot\View\Components\XotBaseComponent;
 
 /**
- * Class Field.
+ * .
  */
 class Card extends Component
 {
-    public function render()
+    public function __construct(
+        public Article $article,
+        public bool $showAuthor = false,
+        public string $tpl = 'v1')
     {
-        $view='blog::article.card';
-        $view_params=[];
-        return view($view,$view_params);
+    }
+
+    public function render(): Renderable
+    {
+        /**
+         * @phpstan-var view-string
+         */
+        $view = app(GetViewAction::class)->execute($this->tpl);
+
+        $view_params = [];
+
+        return view($view, $view_params);
     }
 }
