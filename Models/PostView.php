6607fc11d5df669412d8f339a51c2581f5615c29
marco76tv <marco76tv@users.noreply.github.com>
2023-12-06 18:04:41 +0000

    Lint

--- Models/PostView.php
+++ Models/PostView.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Carbon;
  * @property int         $user_id
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|PostView   newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|PostView   newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|PostView   query()
@@ -27,6 +28,7 @@ use Illuminate\Support\Carbon;
  * @method static \Illuminate\Database\Eloquent\Builder|PostView   whereUserAgent($value)
  * @method static \Illuminate\Database\Eloquent\Builder|PostView   whereUserId($value)
  * @method static \Modules\Blog\Database\Factories\PostViewFactory factory($count = null, $state = [])
+ *
  * @mixin \Eloquent
  */
 class PostView extends EloquentModel
